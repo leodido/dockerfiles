b526ef73fbb32043f01a3de1821b0a4671d60e65
Tim Tisdall <tisdall@gmail.com>
2015-11-17 11:29:18 -0500

    added templates and update script
    Following the pattern at https://github.com/docker-library/postgres
    I've added a Dockerfile.template, README.template.md, and update.sh
    to facilitate easy maintenance of the different sphinx versions.
    
    Adding a new version requires creating a new directory, but after
    that the script will see it and create the needed files.
    
    Note: the script ignores the "latest" version.  I think you can
    change the settings in docker hub to simply tag one of the other
    directories as ":latest" so that directory should probably be
    removed.

---
+++ update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# update sphinxsearch versions
+
+set -eo pipefail
+
+google_re_version='2015-11-01'
+dictionaries=('http://sphinxsearch.com/files/dicts/ru.pak' 'http://sphinxsearch.com/files/dicts/en.pak' 'http://sphinxsearch.com/files/dicts/de.pak')
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( sphinxsearch\:*[0-9a]/ )  # version numbers, beta, but not "latest"
+fi
+versions=( "${versions[@]%/}" )
+versions=( "${versions[@]#sphinxsearch:}" )
+
+# fetch dictionaries into temporary folder, but only if newer
+mkdir -p .dicts
+for dict_url in "${dictionaries[@]}"; do
+	filename="${dict_url##*/}"
+	curl -z ".dicts/$filename" -o ".dicts/$filename" "$dict_url"
+done
+
+for version in "${versions[@]}"; do
+	(
+		if [[ $version == *"beta"* ]]; then
+			sphinx_release_type='beta'
+		else
+			sphinx_release_type='release'			
+		fi
+
+		set -x
+		cp indexall.sh "sphinxsearch:$version/"
+		cp searchd.sh "sphinxsearch:$version/"
+
+		mkdir -p "sphinxsearch:$version/dicts/"
+		cp .dicts/* "sphinxsearch:$version/dicts/"
+		
+		sed 's/%%SPHINX_VERSION%%/'$version'/g; s/%%SPHINX_RELEASE%%/'$sphinx_release_type'/g; s/%%GOOGLE_RE_VERSION%%/'$google_re_version'/g' Dockerfile.template > "sphinxsearch:$version/Dockerfile"
+		sed 's/%%SPHINX_VERSION%%/'$version'/g; s/%%SPHINX_RELEASE%%/'$sphinx_release_type'/g; s/%%GOOGLE_RE_VERSION%%/'$google_re_version'/g' README.template.md > "sphinxsearch:$version/README.md"
+	)
+done
+
